bcdf34a54a23eff026c7205b314c2682bb9800e4
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-07-27 16:25:32 +0530

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,74 @@
+// var a=10
+// var b=30
+
+// function add()
+// {
+//     console.log(a+b)
+// }
+
+// add()
+
+// var factor="Dhaush"
+// var fplayer="Dhoni"
+// var fmovie="Raayan"
+
+// function fav(){
+//     console.log("fav actor:"+"factor")
+//     console.log("fav player:"+"Dhoni")
+//     console.log("fav movie:"+"Raayan")
+// }
+
+// fav()
+
+// function area(len,bre)
+// {
+//     console.log("area is:"+len*bre)
+// }
+
+// area(9,5)
+
+// function add(a,b)
+// {
+//     return(a+b)
+// }
+// var a=add(20,50)  
+// console.log(a)
+
+// var finish=true
+// if (finish)
+//    console.log("Great job")
+// else
+//    console.log("Finish your homework")   
+
+// var left=false
+// if (left)
+//    console.log("would you like a cookie?")
+// else
+//    console.log("Time to bake more cookie.")
+
+// var brush=true
+// if (brush)
+//    console.log("Your teeth are clean")
+// else
+//    console.log("Go Brush your are clean")
+
+// var season="summer"
+// if (season=="spring")
+// {
+//    console.log("Enjoy the blooming flowers")
+// }
+// if (season=="summer")
+//     {console.log("Have fun in the sun")}
+// if (season=="Autumn or fall")
+//     {console.log("Admire the colorful leaves")}
+// if (season=="winter")
+//   {  console.log("Bundle up and stay warm")}
+
+var score=56
+if (score<50)
+{
+    console.log("You need to improve")}
+if (score>50 && score<70)
+   { console.log("Good job")}
+if (score<70)
+    {console.log("Excellent performance")}
